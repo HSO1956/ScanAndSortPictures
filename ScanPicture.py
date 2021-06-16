4ec905d017dad8273aef2424ada9fb03d2c31187
Henri Sohet <HenriSohet@gmail.com>
2021-06-16 10:29:13 +0200

    Version 1

--- ScanPicture.py
+++ ScanPicture.py
@@ -1,33 +1,71 @@
 #ScanPicture.py
 import os
-import csv
+import logging
+import shutil
 from PIL import Image, ExifTags
 from GPSPhoto import gpsphoto
 from geopy.geocoders import Nominatim
-
-
+from geopy.location import Location
 
 if __name__ == "__main__":
-    fichier = open("W:\photos\data.txt", "w")
+    logging.basicConfig(filename='Logfile.log', level=logging.DEBUG)
+    SaveToSql=False
+    try:
+        undefined=0
+        Total=0    
+        geolocator = Nominatim(user_agent="geoapiExercises")
+        rootdir = "W:\photos"
+        rootdir = "J:\photos"   # sur la clef USB
+        
+        for subdir, dirs, files in os.walk(rootdir):
+            i=0
+            for file in files:
+                if file.find("jpg") >2 or file.find("JPG") >2:
+                    Total=Total+1
+                    print("Total=",Total," With GPS= ",Total-undefined)
+                    myImage = subdir + os.sep + file
+                    myYear=os.path.basename(myImage) [:4]
+                   
+                    try:
+                        data = gpsphoto.getGPSData(myImage)
+                        Latitude=str(data['Latitude'])
+                        Longitude=str(data['Longitude'])
+                        location = geolocator.reverse(Latitude+","+Longitude)
+                        TargetDir ="J:/Sorted/WithGPS/"+myYear
     
-    geolocator = Nominatim(user_agent="geoapiExercises")
-    rootdir = "W:\photos"
-    for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-            myImage = subdir + os.sep + file
-            try:
-                data = gpsphoto.getGPSData(myImage)
-                print(data['Latitude'], data['Longitude'])
-                Latitude=str(data['Latitude'])
-                Longitude=str(data['Longitude'])
-                #Latitude = "25.594095"
-                #Longitude = "85.137566"
-  
-                location = geolocator.reverse(Latitude+","+Longitude)
-                # Display
-                Lieu=location
-                # La localisation a été trouvée
-                fichier.write(myImage+" : "+Lieu.address)
-            except:
-                fichier.write(myImage+": --------")
-    fichier.close()
+                    except:
+                        #----------------------------------------------
+                        # Il n'y a pas de données GPS
+                        #----------------------------------------------
+                        TargetDir ="J:/Sorted/NoGPS/"+myYear
+                        undefined=undefined+1
+                    finally:
+                        # Le répertoire n'existe pas
+                        if not os.path.exists(TargetDir):
+                            os.makedirs(TargetDir)
+                        # Move file
+                        try:
+                            shutil.move(myImage,TargetDir)
+                        except:
+                            pass    
+                else:
+                    #---------------------------------
+                    #   Ce n'est pas un fichier JPG
+                    #---------------------------------
+                    pass
+
+                
+        print("Total    = ",Total)
+        print("Undefined= ",undefined)
+        print("Defined  = ",Total-undefined)
+        print("Done.......")
+        exit()
+    except Exception as e:
+
+        logging.DEBUG(str(e))
+        pass
+
+    finally:
+        print("Terminé")
+
+            
